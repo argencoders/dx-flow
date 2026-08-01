e1a0140054cc79246b9e829bc78ca43a16213cf9
Peky <rauldiazsolis@gmail.com>
2026-08-01 12:13:15 -0300

    feat: add DefaultStateKey type for state key serialization

---
+++ src/state-keys.ts
@@ -0,0 +1,5 @@
+/**
+ * Tipo base por omisión para las llaves del estado.
+ * Por restricciones de serialización estándar, solo se permiten cadenas de texto puras.
+ */
+export type DefaultStateKey = string;
